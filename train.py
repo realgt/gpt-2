e2542409bc477d0b76034890487e3df1f1a11d40
timschott <tcs9pk@virginia.edu>
2020-08-11 01:19:41 -0400

    yes debug

--- train.py
+++ train.py
@@ -77,6 +77,8 @@ def main():
         for key, value in hparams.items():
         # do something with value
             hparams[key] = dict2[key]
+    print(type(hparams))
+    print(hparams)
     print(hparams['n_ctx'])
 
     if args.sample_length > hparams.n_ctx:
